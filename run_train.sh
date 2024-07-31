165af771dc20b3c23209bca43e9e03aeefee2a0e
SirHelbo <89070425+SirHelbo@users.noreply.github.com>
2024-07-31 14:48:24 +0200

    Initial push

---
+++ run_train.sh
@@ -0,0 +1,9 @@
+# python3 train.py --dataroot ./datasets/mr2ct_pix2pix_nc1 --name mr2ct_pix2pix_nc1 --model pix2pix --display_id -1 --load_size 266 --input_nc 1 --output_nc 1 --n_epochs 2500 --gpu_ids -1 --dataset_mode aligned
+
+
+
+
+
+
+python3 train.py --dataroot ./datasets/mr2ct --name mr2ct --model autoGAN --display_id -1 --load_size 256 --input_nc 1 --output_nc 1 --n_epochs 2500 --norm batch --batch_size 10 --lr 0.00005 --D_update_freq 1 --netG unet_256 --gpu_ids -1
+
